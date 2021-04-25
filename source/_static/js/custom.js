23b4e1fed3da4726d4990adf7f229a5f35cd613e
Yuri V <balt.voronin+gitlab@gmail.com>
2021-04-25 23:52:45 +1000

    Описание признака по клику/тапу

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -1,11 +1,14 @@
 $(document).ready( function() {
-    // $('ul li p').has('span.uncommon').addClass('uncommon');
-    // $('ul li p').has('span.rare').addClass('rare');
-    // $('ul li p').has('span.alignment').addClass('alignment');
+	$('section > h1 + ul, \
+		section > h2 + ul, \
+		section > h3 + ul, \
+		section > h4 + ul, \
+		section > h5 + ul, \
+		section > h6 + ul').addClass('traits');
 
     // Добавляет иконкам действий заголовок всплывающий при наведение.
     // Невозможно реализовать в Sphinx, так как изображения не имеют нужного аттрибута.
     $("img.action").each(function() {
     	$( this ).attr("title", $( this ).attr("alt"));
     });
-})
+});
