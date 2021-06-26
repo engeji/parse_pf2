3c3095915451562184feeede486843bad84b7ace
Yuri V <balt.voronin+gitlab@gmail.com>
2021-06-26 17:17:26 +1000

    Недостающее из LOCG

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Balt'
 author = 'Balt'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.3'
+release = '1.3.4'
 
 
 # -- General configuration ---------------------------------------------------
