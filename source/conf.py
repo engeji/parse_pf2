4cb09d2df5fd8e2288d2a4594b30b16ea8908357
Yuri V <balt.voronin+gitlab@gmail.com>
2021-11-14 00:29:12 +1000

    Новость на заглавной странице

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Balt'
 author = 'Balt'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.6.1'
 
 
 # -- General configuration ---------------------------------------------------
