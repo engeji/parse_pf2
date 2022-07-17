aead88d44ef08c13217926eeaa2eb516ddc9ea30
Yuri V <balt.voronin+gitlab@gmail.com>
2022-07-17 23:32:47 +1000

    Мелкие исправления

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Yuri "Balt" Voronin'
 author = 'Balt'
 
 # The full version, including alpha/beta/rc tags
-release = '1.9.0'
+release = '1.9.1'
 
 
 # -- General configuration ---------------------------------------------------
